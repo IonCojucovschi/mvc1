6d8ec3c43b14e3a3f613c5ce6a247e5bb0a1fbd5
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-08-22 18:42:56 +0300

    Comit number2

---
+++ WebApl1/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApl1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]///validarile adica cand se va rula programul aceste campuri numai decit vor trebui indeplinite.
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\+",ErrorMessage ="Please enter a valid email addres")]// aici se arata forma care trebuie sa o aiba emailul pentru a fi acceptat.
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
